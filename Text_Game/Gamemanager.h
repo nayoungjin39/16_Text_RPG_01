b01d4ff6f5a613a2e16fd36daaaea071575113ad
nayoungjin39 <211291869+nayoungjin39@users.noreply.github.com>
2025-06-20 17:02:40 +0900

    Gamemanager

--- Text_Game/Gamemanager.h
+++ Text_Game/Gamemanager.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include "Character.h"
+#include "Monster.h"
+
+class Gamemanager {
+public:
+    Monster* Generatemonster(int level);
+    void Battle(Character* player);
+    void DisplayInventory(Character* player);
+    int GetRandomInt(int min, int max);
+};
